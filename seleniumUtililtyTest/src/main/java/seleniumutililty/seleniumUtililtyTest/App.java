d2408484e9e2d44cc279d84be331e575074a8840
msapna717 <msapna717@gmail.com>
2019-12-27 20:04:39 +0530

    initial commit

---
+++ seleniumUtililtyTest/src/main/java/seleniumutililty/seleniumUtililtyTest/App.java
@@ -0,0 +1,13 @@
+package seleniumutililty.seleniumUtililtyTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
